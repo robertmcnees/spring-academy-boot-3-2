87081cd615da3adebcae5e5723329640b0a4d300
Robert McNees <rmcnees@vmware.com>
2024-02-12 08:12:23 -0500

    Added import for JdkClientHttpRequestFactory

--- sample-client/src/main/java/academy/spring/sample/client/controller/ClientController.java
+++ sample-client/src/main/java/academy/spring/sample/client/controller/ClientController.java
@@ -1,6 +1,7 @@
 package academy.spring.sample.client.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
